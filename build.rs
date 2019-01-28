514e8688a0f029bf92d78de39aeb25117702c4ac
uuhan <xuminhui189@gmail.com>
2019-01-28 22:57:27 +0800

    WIP

---
+++ build.rs
@@ -0,0 +1,7 @@
+use cmake;
+
+fn main() {
+    let dst = cmake::build("cat");
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=catclient");
+}

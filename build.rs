2fdf565e6d9eddb5b6e4c5a976780d9fb7dafd88
uuhan <xuminhui189@gmail.com>
2019-01-30 14:02:10 +0800

    use https://github.com/dianping/cat as submodule

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use cmake;
 
 fn main() {
-    let dst = cmake::build("cat");
+    let dst = cmake::build("./cat-sys/lib/c");
     println!("cargo:rustc-link-search=native={}/lib/", dst.display());
     println!("cargo:rustc-link-lib=static=catclient");
 }
--- cat/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-## 3.x
-
-## 3.0.x
-
-### 3.0.1
-
-- Fixed a bug that caused segmentation fault while getting local ip.

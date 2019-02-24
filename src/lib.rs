d12956025fe20f85c94eb67e7292d299a55d9022
uuhan <xuminhui189@gmail.com>
2019-02-24 23:00:38 +0800

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,36 @@ pub fn cat_client_init(appkey: String) -> i32 {
 pub fn cat_client_init_with_config(appkey: String, config: &mut ffi::CatClientConfig) -> i32 {
     unsafe { ffi::catClientInitWithConfig(c!(appkey), config) }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::CatClient;
+    use super::CatTransaction;
+    use std::ptr;
+
+    #[test]
+    fn test_transaction() {
+        let mut cat = CatClient::new("test");
+        cat.init();
+        let version = cat.version();
+        let tr = CatTransaction::new("foo".to_owned(), "bar".to_owned());
+
+        assert!(!tr.is_null());
+
+        unsafe {
+            super::logEvent(
+                b"foo\0".as_ptr() as *const u8,
+                b"bar\0".as_ptr() as *const u8,
+                b"0\0".as_ptr() as *const u8,
+                ptr::null(),
+            );
+            super::logEvent(
+                b"foo\0".as_ptr() as *const u8,
+                b"bar\0".as_ptr() as *const u8,
+                b"1\0".as_ptr() as *const u8,
+                ptr::null(),
+            );
+            (*tr).complete();
+        }
+    }
+}

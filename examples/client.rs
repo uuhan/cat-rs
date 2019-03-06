21da625376760c12cde196b2200c00b7c85612d9
uuhan <xuminhui189@gmail.com>
2019-03-07 02:09:05 +0800

    fix: test

--- examples/client.rs
+++ examples/client.rs
@@ -5,7 +5,6 @@ extern crate threadpool;
 use std::alloc::System;
 use std::error::Error;
 use std::result::Result;
-use std::thread::sleep_ms;
 
 use threadpool::ThreadPool;
 
@@ -15,19 +14,15 @@ static GLOBAL: System = System;
 use cat::logEvent;
 use cat::CatClient;
 use cat::CatTransaction;
-use cat::CatTransactionService;
 
 pub fn main() -> Result<(), Box<Error>> {
     let mut cat = CatClient::new("test");
     cat.init()?;
 
-    let mut trs = CatTransactionService::new().pool_size(4).init();
-    let mut tr = trs.create("foo", "bar");
+    let mut tr = CatTransaction::new("foo", "bar");
 
-    tr.logEvent("test", "it", "0", " ");
+    tr.log("test", "it", "0", " ");
     tr.complete();
-
-    trs.pool.unwrap().join();
 
     Ok(())
 }
